905bc31dd42b4fd9c891e2f84134a17880838bcd
aalborghetti <alberto.alborghetti@gmail.com>
2022-05-03 22:15:18 +0200

    sudoku

---
+++ script.js
@@ -0,0 +1,103 @@
+//Script
+
+
+//alert(pippo);
+function newGame () {
+    alert("new");
+}
+
+function setNumberA1() {
+    const a1 = document.getElementById("a1");
+    number = document.querySelector('input[name="number"]:checked').value;
+    a1.innerHTML = number;
+}
+
+function setNumberA2() {
+    const a2 = document.getElementById("a2");
+    number = document.querySelector('input[name="number"]:checked').value;
+    a2.innerHTML = number;
+}
+
+function setNumberA3() {
+    const a3 = document.getElementById("a3");
+    number = document.querySelector('input[name="number"]:checked').value;
+    a3.innerHTML = number;
+}
+
+function setNumberA4() {
+    const a4 = document.getElementById("a4");
+    number = document.querySelector('input[name="number"]:checked').value;
+    a4.innerHTML = number;
+}
+
+function setNumberB1() {
+    const b1 = document.getElementById("b1");
+    number = document.querySelector('input[name="number"]:checked').value;
+    b1.innerHTML = number;
+}
+
+function setNumberB2() {
+    const b2 = document.getElementById("b2");
+    number = document.querySelector('input[name="number"]:checked').value;
+    b2.innerHTML = number;
+}
+
+function setNumberB3() {
+    const b3 = document.getElementById("b3");
+    number = document.querySelector('input[name="number"]:checked').value;
+    b3.innerHTML = number;
+}
+
+function setNumberB4() {
+    const b4 = document.getElementById("b4");
+    number = document.querySelector('input[name="number"]:checked').value;
+    b4.innerHTML = number;
+}
+
+function setNumberC1() {
+    const c1 = document.getElementById("c1");
+    number = document.querySelector('input[name="number"]:checked').value;
+    c1.innerHTML = number;
+}
+
+function setNumberC2() {
+    const c2 = document.getElementById("c2");
+    number = document.querySelector('input[name="number"]:checked').value;
+    c2.innerHTML = number;
+}
+
+function setNumberC3() {
+    const c3 = document.getElementById("c3");
+    number = document.querySelector('input[name="number"]:checked').value;
+    c3.innerHTML = number;
+}
+
+function setNumberC4() {
+    const c4 = document.getElementById("c4");
+    number = document.querySelector('input[name="number"]:checked').value;
+    c4.innerHTML = number;
+}
+
+function setNumberD1() {
+    const d1 = document.getElementById("d1");
+    number = document.querySelector('input[name="number"]:checked').value;
+    d1.innerHTML = number;
+}
+
+function setNumberD2() {
+    const d2 = document.getElementById("d2");
+    number = document.querySelector('input[name="number"]:checked').value;
+    d2.innerHTML = number;
+}
+
+function setNumberD3() {
+    const d3 = document.getElementById("d3");
+    number = document.querySelector('input[name="number"]:checked').value;
+    d3.innerHTML = number;
+}
+
+function setNumberD4() {
+    const d4 = document.getElementById("d4");
+    number = document.querySelector('input[name="number"]:checked').value;
+    d4.innerHTML = number;
+}
